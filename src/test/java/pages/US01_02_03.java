dc8505dbc920c13a2b2f5173ee14a68ebd632424
Sefa Tohumcu <sefatohumcu87.st@gmail.com>
2022-11-16 20:53:36 +0100

    V1

---
+++ src/test/java/pages/US01_02_03.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US01_02_03 {
+
+
+    public US01_02_03()  {
+        PageFactory.initElements(Driver.getDriver(), this);}
+
+
+
+
+}
